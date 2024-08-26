7fe49662f952a0483bf959fed61c883db49ef889
Hithesh Jain <114323232+hitheshjain@users.noreply.github.com>
2024-08-26 12:33:42 +0530

    ok

---
+++ INCEPTION/script.js
@@ -0,0 +1,19 @@
+const heading = React.createElement(
+  "h1",
+  { id: "heading" },
+  "hello world from react"
+);
+const parent = React.createElement(
+  "div",
+  { id: "parent" },
+  [React.createElement("div", { id: "child" }, [
+    React.createElement("h1", {}, "child heading"),
+    React.createElement("h2", {}, "child  2 heading"),
+  ]),React.createElement("div", { id: "child2" }, [
+    React.createElement("h1", {}, "child heading"),
+    React.createElement("h2", {}, "child  4 heading"),
+  ])
+]
+);
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(parent);
