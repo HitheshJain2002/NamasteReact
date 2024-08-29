af73deb65543eec1cfe55ef43a21121426ccd20d
Hithesh Jain <114323232+hitheshjain@users.noreply.github.com>
2024-08-29 23:51:24 +0530

    json data and automtic card fill

--- project1/App.js
+++ project1/App.js
@@ -1,13 +1,27 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
+const elements=<span>hello world gg oggg hithnesh</span>;
 // React Element
-const heading = React.createElement(
-    "h1",
-    { id: "heading" },
-    "NAMASTE REACT🚀🚀"
+const Title = (
+    
+   <h1 className="head"> {elements}title </h1>
 );
-
 // Render to the root element
+
+//jsx
+
+const number=1000;
+
+//react  functionalcomponent
+const HeadComponent = () => (
+    <div id="container">
+     {Title}
+      <h2>{number}</h2>
+      <h1 className="heading">hello world using functionalcomponent</h1>
+    </div>
+  );
+  
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(heading);
+root.render(<HeadComponent />);
+ 
